4cba64fd01bd8e4f17224bf49b357ba14a2072ea
SamayGitHub <145737132+SamayGitHub@users.noreply.github.com>
2023-09-22 13:21:57 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title ('Hello samay welcome to new home')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard Boiled free range egg')
+streamlit.title ('Samay New Healty Breakfast')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard Boiled free range egg')
+streamlit.text('🥑🍞Avocado Toast')
