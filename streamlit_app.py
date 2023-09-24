0f1994da58f7b59169ebf7326192a84ad309c4c6
SamayGitHub <145737132+SamayGitHub@users.noreply.github.com>
2023-09-24 19:23:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ streamlit.header("The Fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("select * from fruit_load_list")
-  return my_cur.fetchall()
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
 
 #Add a button to load a fruit
 if streamlit.button("Get Fruit load list"):
